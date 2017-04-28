7ff85e2c92db93498b6f102c4c579842bd7e6c10
PartMent <partment@users.noreply.github.com>
2017-04-28 09:09:07 +0800

    Fix the button of changing language

--- assets/common.js
+++ assets/common.js
@@ -2,10 +2,11 @@ $(window).load(function () {
     setTimeout(function() {
         $(".loading").stop().fadeOut(700);
         $(".wrap").stop().fadeIn(700);
+        $(".lang").stop().fadeIn(700);
     }, 200);
+    setlang("tw");
 });
 $(function () {
-    setlang("tw");
     $(".lang").click(function () {
         if( $(this).data("region") === "tw") {
             setlang("us");
