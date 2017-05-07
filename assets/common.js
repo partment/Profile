1fadf969671a74b3ec53f4eac32d155e68b3a360
PartMent <partment@users.noreply.github.com>
2017-05-07 02:35:31 -0500

    Fix mobile interface

--- assets/common.js
+++ assets/common.js
@@ -18,15 +18,15 @@ $(function () {
         return false;
     });
     if(is.mobile()) {
-        $(".wrap").css({"width":"100%","min-width":"100%","height":"400px"});
-        $(".info").css("height","300px");
+        $(".wrap").css({"width":"100%","min-width":"100%","height":"450px"});
+        $(".info").css("height","350px");
         $(".info-icon").css({"width":"150px","height":"150px","background-size":"120px 120px"});
-        $(".info-description").css({"font-size":"13px","transform":"translate(-50%, calc(-50% - 80px))"});
+        $(".info-description").css({"font-size":"14px","height":"200px"});
     }
 });
 function setlang (region) {
     $(".title").html(lang[region+'-title']);
     $(".lang").html(lang[region+'-lang']).data("region", region);
-    $(".info-description").html(lang[region+'-description']);
+    $(".info-description-content").html(lang[region+'-description']);
     return;
 }
